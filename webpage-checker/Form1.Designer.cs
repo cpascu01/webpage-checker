19eafc8a39cbf316e0d244221e2370518f321886
Chris Pascual <christopher.pascual@gmail.com>
2021-11-30 00:47:08 -0500

    Initial commit

--- webpage-checker/Form1.Designer.cs
+++ webpage-checker/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(560, 221);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Compare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(150, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 332);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- webpage-checker/Form1.cs
+++ webpage-checker/Form1.cs
@@ -1,21 +1,114 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml;
+using WDSE;
+using WDSE.Decorators;
+using WDSE.ScreenshotMaker;
 
 namespace webpage_checker
 {
     public partial class Form1 : Form
     {
+        IWebDriver _driver;
         public Form1()
         {
             InitializeComponent();
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Size = new Size(1300, 1080);
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> urlList = readSitemap();
+
+
+            ScreenshotMaker screenmaker = new ScreenshotMaker();
+            screenmaker.RemoveScrollBarsWhileShooting();
+            VerticalCombineDecorator vcd = new VerticalCombineDecorator(screenmaker);
+
+            progressBar1.Maximum = urlList.Count;
+            foreach (string url in urlList)
+            {
+                _driver.Navigate().GoToUrl(url);
+                
+                var screen = _driver.TakeScreenshot(vcd);
+                string filename = string.Format("{0}{1}", url.Replace("https://", "").Replace("/", "--"), ".png");
+                File.WriteAllBytes(filename, screen);
+
+                progressBar1.Value++;
+            }
+            _driver.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string img1_ref, img2_ref;
+            int count1 = 0, count2 = 0;
+            bool flag = true;
+            var img1 = new Bitmap("fullimage.png");
+            var img2 = new Bitmap("fullimage2.png");
+            progressBar1.Maximum = img1.Width;
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+                        if (img1_ref != img2_ref)
+                        {
+                            count2 = +1;
+                            flag = false;
+                            break;
+                        }
+                        count1 = +1;
+                    }
+                    progressBar1.Value++;
+                }
+                if (flag == false)
+                    MessageBox.Show("Sorry, Images are not same , " + count2 + " wrong pixels found");
+                else
+                    MessageBox.Show(" Images are same , " + count1 + " same pixels found and " + count2 + " wrong pixels found");
+            }
+            else
+                MessageBox.Show("can not compare this images");
+        }
+
+        private List<string> readSitemap()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            
+            FileStream fs = new FileStream("sitemap.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+
+            XmlNodeList urlNodes = xmldoc.GetElementsByTagName("url");
+            List<string> urls = new List<string>();
+            foreach (XmlNode url in urlNodes)
+            {
+                string location = url["loc"].InnerText;
+                urls.Add(location);
+            }
+
+            fs.Close();
+            fs.Dispose();
+
+            return urls;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _driver.Quit();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
     }
 }
